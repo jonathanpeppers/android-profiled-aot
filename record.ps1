011ba762afb6248e451e09d54de6f3be05618980
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-09-01 09:58:25 -0500

    Setup aotprofile-tool

--- record.ps1
+++ record.ps1
@@ -16,3 +16,14 @@ Start-Sleep -Seconds $seconds
 # Pull the custom.aprof file from the device
 & $dotnet build $csproj -t:FinishAotProfiling -bl:logs/$app-FinishAotProfiling.binlog
 
+# Build aotprofile-tool if needed
+$aotprofile_tool = "external/aotprofile-tool/bin/Debug/aotprofile-tool"
+if (-Not (Test-Path $aotprofile_tool))
+{
+    & nuget restore external/aotprofile-tool
+    & $dotnet build external/aotprofile-tool -bl:logs/aotprofile-tool.binlog
+}
+
+# Strip out the user's assembly
+$custom_aprof = "$app/custom.aprof"
+& $aotprofile_tool -sd --filter-module="^(?!$app).+" $custom_aprof -o profiles/$app.aotprofile
