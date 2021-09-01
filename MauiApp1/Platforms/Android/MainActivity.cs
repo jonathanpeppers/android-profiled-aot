ab88d79f22adef87bdcb7eaa9dd386cb65eac02a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-09-01 14:15:21 -0500

    `dotnet new maui` project template
    Removed everything that wasn't Android

---
+++ MauiApp1/Platforms/Android/MainActivity.cs
@@ -0,0 +1,13 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Microsoft.Maui;
+
+namespace MauiApp1
+{
+	[Register("MauiApp1.MainActivity")]
+	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+	public class MainActivity : MauiAppCompatActivity
+	{
+	}
+}
