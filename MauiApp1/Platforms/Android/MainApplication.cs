16856ab38f4fe3e7e890f596abfee835ac0ccc05
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-01-27 11:09:12 -0600

    Updated latest maui template

--- MauiApp1/Platforms/Android/MainApplication.cs
+++ MauiApp1/Platforms/Android/MainApplication.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Runtime;
-using Microsoft.Maui;
 
-namespace MauiApp1
+namespace MauiApp1;
+
+[Application]
+public class MainApplication : MauiApplication
 {
-	[Application]
-	public class MainApplication : MauiApplication
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
 	{
-		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-			: base(handle, ownership)
-		{
-		}
-
-		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
